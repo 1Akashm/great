ef3a2b899f3f3274fcd85baa602a49e7086f8902
1Akashm <magarakash183@gmail.com>
2024-03-21 11:55:34 +0545

    updated

--- index.js
+++ index.js
@@ -17,26 +17,12 @@ document.addEventListener("DOMContentLoaded", () => {
 // to know the activated list item
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll(".ul_container ul li a");
-
   links.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
-
-      // Check if the link is the "Home" link
       const isHomeLink = link.classList.contains("fontactivate");
-
-      // Remove the fontactivate class from all links
       links.forEach((l) => l.classList.remove("fontactivate"));
-
-      // Add the fontactivate class to the clicked link
       link.classList.add("fontactivate");
-
-
     });
   });
 });
-
-
-
-
-
